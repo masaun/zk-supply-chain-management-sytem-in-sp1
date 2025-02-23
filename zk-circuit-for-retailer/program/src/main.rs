4e03742b428668f51cd595199a4ba7c4c3f28e6f
masaun <masaun@protonmail.com>
2025-02-23 12:10:02 +0900

    [program (for Retailer)]: Define the PurchaseOrder struct

--- zk-circuit-for-retailer/program/src/main.rs
+++ zk-circuit-for-retailer/program/src/main.rs
@@ -12,55 +12,31 @@ sp1_zkvm::entrypoint!(main);
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-struct ProductDetail {
-//struct RfidData { // RFID data
-    serial_number: u32,
-    pub upc_or_ean: u64, // Product Code (1) - UPC or EAN barcode (12 ~ 13 digits)
-    pub gtin: u64,       // Product Code (2) - GTIN-12 code (12 digits)
-    product_description: String,
+struct PurchaseOrder {
+    product_id: u32,
+    purchase_order_id: u32,
     batch_number: u16,    // Same with "Lot number"
     is_food: u8,
-    //is_food: bool,          // for Food
-    expiration_date: u32,  // for Food (in timestamp)
-    temperature: u8,     // for Food. The templature that should be maintained at.
-    location_origin: String,          // i.e. "New York" -> Field type -> Slice it + Measure length of it -> Array Field type ([u8; N])
     location_destination: String,     // i.e. "London"
     order_date: u32,      // Timestamp
-    shipping_date: u32,   // Timestamp
-    supplier_ids: u32,
-    supplier_certificates: u32,
-    supplier_signatures: String,
-    supplier_wallet_addresses: String
 }
 
 pub fn main() {
     // @dev - Read the input data (input stream) from RFID
-    let product_detail: ProductDetail = sp1_zkvm::io::read::<ProductDetail>();
-    //let rfid_data: RfidData = sp1_zkvm::io::read::<RfidData>();
-    let supplier_name: String = sp1_zkvm::io::read::<String>(); // Shuld be the "private" input (Not to be commited as a public value. This value keep a "private" state)
+    let purchase_order: PurchaseOrder = sp1_zkvm::io::read::<PurchaseOrder>();
+    //let supplier_name: String = sp1_zkvm::io::read::<String>(); // Shuld be the "private" input (Not to be commited as a public value. This value keep a "private" state)
 
-    println!("ProductDetail - serial_number: {}", product_detail.serial_number);
-    println!("ProductDetail - upc_or_ean: {}", product_detail.upc_or_ean);
-    println!("ProductDetail - gtin: {}", product_detail.gtin);
-    println!("ProductDetail - product_description: {}", product_detail.product_description);
-    println!("ProductDetail - batch_number: {}", product_detail.batch_number);
-    println!("ProductDetail - is_food: {}", product_detail.is_food);
-    println!("ProductDetail - expiration_date: {}", product_detail.expiration_date);
-    println!("ProductDetail - temperature: {}", product_detail.temperature);
-    println!("ProductDetail - location_origin: {}", product_detail.location_origin);
-    println!("ProductDetail - location_destination: {}", product_detail.location_destination);
-    println!("ProductDetail - order_date: {}", product_detail.order_date);
-    println!("ProductDetail - shipping_date: {}", product_detail.shipping_date);
-    println!("ProductDetail - supplier_ids: {}", product_detail.supplier_ids);
-    println!("ProductDetail - supplier_certificates: {}", product_detail.supplier_certificates);
-    println!("ProductDetail - supplier_signatures: {}", product_detail.supplier_signatures);
-    println!("ProductDetail - supplier_wallet_addresses: {}", product_detail.supplier_wallet_addresses);
+    println!("PurchaseOrder - product_id: {}", purchase_order.product_id);
+    println!("PurchaseOrder - location_destination: {}", purchase_order.location_destination);
+    println!("PurchaseOrder - batch_number: {}", purchase_order.batch_number);
+    println!("PurchaseOrder - is_food: {}", purchase_order.is_food);
+    println!("PurchaseOrder - location_destination: {}", purchase_order.location_destination);
+    println!("PurchaseOrder - order_date: {}", purchase_order.order_date);
 
-    println!("Supplier Name (private state): {}", supplier_name);  // Shuld be the "private Output" (Not to be commited as a public value)
+    //println!("Supplier Name (private state): {}", supplier_name);  // Shuld be the "private Output" (Not to be commited as a public value)
 
     // Write the result (true or false) to the output. (NOTE: Only value to be "public Output" should be commited)
-    sp1_zkvm::io::commit(&product_detail.upc_or_ean);
-    sp1_zkvm::io::commit(&product_detail.gtin);
+    sp1_zkvm::io::commit(&product_detail.product_id);
+    sp1_zkvm::io::commit(&product_detail.purchase_order_id);
     sp1_zkvm::io::commit(&product_detail.is_food);
-    //sp1_zkvm::io::commit(&rfid_data);
 }
